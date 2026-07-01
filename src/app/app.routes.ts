079dabc9a2b25fa62a4d4b58514e67634668b13e
Alexander <alexanderxavierneyradelacruz@gmail.com>
2026-07-01 13:34:18 -0500

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,29 +3,29 @@ import { Home } from './pages/home/home';
 import { Login } from './pages/login/login';
 import { Registro } from './pages/registro/registro';
 import { DashboardCliente } from './pages/dashboard-cliente/dashboard-cliente';
-import { DashboardAdmin } from './pages/dashboard-admin/dashboard-admin';
+//import { DashboardAdmin } from './pages/dashboard-admin/dashboard-admin';
 import { Barberias } from './pages/barberias/barberias';
 import { Equipo } from './pages/equipo/equipo';
 import { Productos } from './pages/productos/productos';
-import { Confirmacion } from './pages/confirmacion/confirmacion';
-import { GestionProductos } from './pages/dashboard-admin/gestion-productos/gestion-productos';
-import { GestionEmpleados } from './pages/dashboard-admin/gestion-empleados/gestion-empleados';
-import { GestionCitas } from './pages/dashboard-barbero/gestion-citas/gestion-citas';
-import { MisCitas } from './pages/mis-citas/mis-citas';
+//import { Confirmacion } from './pages/confirmacion/confirmacion';
+//import { GestionProductos } from './pages/dashboard-admin/gestion-productos/gestion-productos';
+//import { GestionEmpleados } from './pages/dashboard-admin/gestion-empleados/gestion-empleados';
+//import { GestionCitas } from './pages/dashboard-barbero/gestion-citas/gestion-citas';
+//import { MisCitas } from './pages/mis-citas/mis-citas';
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: Home },
   { path: 'login', component: Login },
   { path: 'registro', component: Registro },
   { path: 'dashboard-cliente', component: DashboardCliente },
-  { path: 'dashboard-admin', component: DashboardAdmin },
+ // { path: 'dashboard-admin', component: DashboardAdmin },
  { path: 'barberias', component: Barberias },
  { path: 'equipo', component: Equipo },
- { path: 'confirmacion', component: Confirmacion },
+ //{ path: 'confirmacion', component: Confirmacion },
  { path: 'productos', component: Productos },
- { path: 'mis-citas', component: MisCitas },
- { path: 'gestion-productos', component: GestionProductos },
-{ path: 'gestion-empleados', component: GestionEmpleados },
- { path: 'gestion-citas', component: GestionCitas },
+// { path: 'mis-citas', component: MisCitas },
+ //{ path: 'gestion-productos', component: GestionProductos },
+//{ path: 'gestion-empleados', component: GestionEmpleados },
+// { path: 'gestion-citas', component: GestionCitas },
   { path: '**', redirectTo: 'home' }
 ];
